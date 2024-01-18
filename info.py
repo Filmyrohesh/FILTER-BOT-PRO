d08d66f4660f378803a38677a7e258132a7e546e
Rohesh-Gavit <153485430+Rohesh-Gavit@users.noreply.github.com>
2024-01-18 22:06:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -80,11 +80,11 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Telegram")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
 
 # Links
-SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/HA_Bots_Support')
-UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/HA_Bots')
-FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/HA_Films_World')
-TUTORIAL = environ.get("TUTORIAL", "https://t.me/HA_Bots")
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/HA_Bots")
+SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/MKV_FILES_ROHESH')
+UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/Filmy_Rohesh')
+FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/Movie_Searching_Pro')
+TUTORIAL = environ.get("TUTORIAL", "https://t.me/Filmy_Rohesh")
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/Filmy_Rohesh")
 
 # Bot settings
 DELETE_TIME = int(environ.get('DELETE_TIME', 3600)) # Add time in seconds
