79a1d01abbdfd7778f0e5be758a882043a236833
Filmyrohesh <142705544+Filmyrohesh@users.noreply.github.com>
2024-01-30 18:34:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -59,7 +59,7 @@ else:
     LOG_CHANNEL = int(LOG_CHANNEL)
 
 # support group
-SUPPORT_GROUP = environ.get('SUPPORT_GROUP', '-1001792853580')
+SUPPORT_GROUP = environ.get('SUPPORT_GROUP', '')
 if len(SUPPORT_GROUP) == 0:
     print('Error - SUPPORT_GROUP is missing, exiting now')
     exit()
